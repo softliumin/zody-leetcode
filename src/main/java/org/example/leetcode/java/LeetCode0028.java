785084aa19f44578dbd1e7c868606712d82fc6c0
柳敏 <liu_min@ypshengxian.com>
2020-07-02 20:40:23 +0800

    clean code

--- src/main/java/org/example/leetcode/java/LeetCode0028.java
+++ src/main/java/org/example/leetcode/java/LeetCode0028.java
@@ -1,17 +1,11 @@
-/**
- * @(#)LeetCode0028.java, 2020/7/2.
- * <p/>
- * Copyright 2020 Netease, Inc. All rights reserved.
- * NETEASE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 package org.example.leetcode.java;
 
 /**
- * @author 柳敏 liumin06@corp.netease.com
+ * @author zody
  * @since 2020-07-02 20:35
  */
 public class LeetCode0028 {
-    
+
     public static void main(String[] args) {
 
     }
